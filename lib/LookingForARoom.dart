dd61397ca9c221efe17742790d2ff1d9971f1971
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-09 20:16:40 +0530

    initial commit

---
+++ lib/LookingForARoom.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(home: LookingForARoom()));
+}
+
+class LookingForARoom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:homie/LookingForARoom.dart';
+
+void main() {
+  runApp(MaterialApp(home: Homescreen()));
+}
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 10,
+            bottom: 0,
+            child: Container(
+              height: 400,
+              alignment: Alignment(0,-1),
+              child: Image(
+                image: AssetImage('assets/logo.png'),),
+            ),
+          ),
+          Positioned(
+            child: Container(
+              alignment: Alignment(0,0),
+              child: RaisedButton(
+                  padding: EdgeInsets.fromLTRB(35, 8, 35, 8),
+                  color: Color(0xff5F34A7),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>LookingForARoom()));
+                  },
+                  child: Text('Looking for a room ?',
+                      style: TextStyle(fontSize: 20, color: Colors.white))
+              ),
+            ),
+          ),
+          Positioned(
+            child: Container(
+              child: Text('Post your requirements and a room owner will find you! ',
+                style: TextStyle(fontSize: 12, color: Colors.black),textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+
+          Positioned(
+            child: Container(
+              child: RaisedButton(
+                  padding: EdgeInsets.fromLTRB(14, 8, 14, 8),
+                  color: Color(0xff5F34A7),
+                  onPressed: () {},
+                  child: Text('Looking for a roommate ?',
+                      style: TextStyle(fontSize: 20, color: Colors.white))),
+            ),
+          ),
+
+          Positioned(
+              child: Container(
+                alignment: Alignment(0,3),
+                child: Text('Post your property details and a user will find you! ',
+                  style: TextStyle(fontSize: 12, color: Colors.black),textAlign: TextAlign.end,
+                ),
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
