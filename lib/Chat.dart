6d77729176fa67b7093faaac0ba1d9d6b6b0c2da
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-11 12:03:40 +0530

    chat partial done with http request

--- lib/Chat.dart
+++ lib/Chat.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:homie/reuseable_search_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() => runApp(Chat());
 
@@ -9,23 +12,56 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
+  String url = "https://randomuser.me/api/?results=15";
+  List data;
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var extractdata = json.decode(response.body);
+      data = extractdata["results"];
+    });
+  }
+
+  @override
+  void initState() {
+    this.makeRequest();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         //TODO: bottom padding resize error
         body: new Column(
-          children: [
-            new Expanded(
-              flex: 1,
-              child: SearchCard(),
-            ),
-            new Expanded(flex: 4, child: new Container(child: DummyList())),
-          ],
-        ));
+      children: [
+        new Expanded(
+          flex: 1,
+          child: SearchCard(),
+        ),
+        new Expanded(
+            flex: 4,
+            child: new Container(
+                child: new Scaffold(
+              body: new ListView.builder(
+                  itemCount: data == null ? 0 : data.length,
+                  itemBuilder: (BuildContext context, i) {
+                    return new ListTile(
+                      title: new Text(data[i]["name"]["first"]),
+                      subtitle: new Text(data[i]["phone"]),
+                      leading: new CircleAvatar(
+                        backgroundImage:
+                            new NetworkImage(data[i]["picture"]["thumbnail"]),
+                      ),
+                    );
+                  }),
+            ))),
+      ],
+    ));
   }
 }
 
-class DummyList extends StatelessWidget {
+/* class DummyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
@@ -41,3 +77,4 @@ class DummyList extends StatelessWidget {
     );
   }
 }
+ */
