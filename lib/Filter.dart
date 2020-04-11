c8cb975c3b9a8a47112275240fd53a615711fc8a
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-11 22:22:25 +0530

    filter half complete without state mgmt

--- lib/Filter.dart
+++ lib/Filter.dart
@@ -4,6 +4,7 @@ class Filter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: new AppBar(
           automaticallyImplyLeading: false,
           backgroundColor: Colors.white,
@@ -38,10 +39,95 @@ class FilterItems extends StatefulWidget {
 }
 
 class _FilterItemsState extends State<FilterItems> {
+  List<bool> isSelected = [false, false, false];
+  RangeValues _values = RangeValues(5, 60);//for budget slider
+  double _value = 18;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+            child: Column(
+          children: [
+            Text("HOUSE TYPE"),
+            ToggleButtons(
+              selectedBorderColor: Colors.purple[900],
+              selectedColor: Colors.purple[900],
+              children: <Widget>[
+                Column(
+                  children: [Icon(Icons.person), Text("Room")],
+                ),
+                Column(
+                  children: [Icon(Icons.group), Text("Twin Sharing")],
+                ),
+                Column(
+                  children: [Icon(Icons.group_add), Text("Multi-Sharing")],
+                )
+              ],
+              isSelected: isSelected,
+              onPressed: (int index) {
+                setState(() {
+                  isSelected[index] = !isSelected[index];
+                });
+              },
+            )
+          ],
+        )),
+        Container(
+          child: Column(
+            children: [
+              Text("Budget"),
+              RangeSlider(
+                values: _values,
+                activeColor: Colors.purple[900],
+                labels: RangeLabels(_values.start.toInt().toString(),
+                    _values.end.toInt().toString()),
+                min: 5,
+                max: 60,
+                divisions: 11,
+                onChanged: (RangeValues values) {
+                  setState(() {
+                    if (values.end - values.start >= 20) {
+                      _values = values;
+                    } else {
+                      if (_values.start == values.start) {
+                        _values =
+                            RangeValues(_values.start, _values.start + 20);
+                      } else {
+                        _values = RangeValues(_values.end - 20, _values.end);
+                      }
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          child: Column(
+            children: [
+              Text("Age"),
+              Slider(
+                onChanged: (value) {
+                  setState(() {
+                    _value = value;
+                  });
+                },
+                min: 18,
+                max: 60,
+                divisions: 42,
+                value: _value,
+                activeColor: Colors.purple[900],
+                label: _value.toInt().toString(),
+                
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
