d8382afa8ebc92d76a121691592c20a24b31419a
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-12 08:17:30 +0530

    broken filter due to grid view

--- lib/Filter.dart
+++ lib/Filter.dart
@@ -39,15 +39,38 @@ class FilterItems extends StatefulWidget {
 }
 
 class _FilterItemsState extends State<FilterItems> {
+  //for gridview
+  Item item1 = new Item(image: "AC", title: "assets/oven.png");
+  Item item2 = new Item(image: "WI-FI", title: "assets/wifi.png");
+  Item item3 = new Item(image: "Fridge", title: "assets/fridge.png");
+  Item item4 = new Item(image: "Washing Machine", title: "assets/heater.png");
+  Item item5 = new Item(image: "Water Heater", title: "assets/heater.png");
+  Item item6 = new Item(image: "T.V", title: "assets/tv.png");
+  Item item7 = new Item(image: "Microwave Oven", title: "assets/oven.png");
+  Item item8 = new Item(image: "R.O Water", title: "assets/ro.png");
+  Item item9 = new Item(image: "Parking", title: "assets/parking.png");
+  //end of grid view vars
   List<bool> isSelected = [false, false, false];
-  RangeValues _values = RangeValues(5, 60);//for budget slider
+  RangeValues _values = RangeValues(5, 60); //for budget slider
   double _value = 18;
+  bool _isRadioSelected = true;
 
   @override
   Widget build(BuildContext context) {
+    List<Item> myList = [
+      item1,
+      item2,
+      item3,
+      item4,
+      item5,
+      item6,
+      item7,
+      item8,
+      item9
+    ];
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
             child: Column(
@@ -122,12 +145,68 @@ class _FilterItemsState extends State<FilterItems> {
                 value: _value,
                 activeColor: Colors.purple[900],
                 label: _value.toInt().toString(),
-                
               ),
             ],
           ),
+        ),
+        Container(
+          child: Column(
+            children: [
+              Text("Sex"),
+              RadioListTile(
+                title: const Text('Male'),
+                value: true,
+                groupValue: _isRadioSelected,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _isRadioSelected = newValue;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: const Text('Female'),
+                value: false,
+                groupValue: _isRadioSelected,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _isRadioSelected = newValue;
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+        Container(
+          child: Column(
+            children: [
+              Text("Ammeneties"),
+              Flexible(
+                child: GridView.count(
+                  childAspectRatio: 1.0,
+                  crossAxisCount: 3,
+                  children: myList.map((data) {
+                    return Container(
+                      child: Column(
+                        children: [
+                          Image.asset(data.image, width: 20),
+                          Text(data.title)
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
         )
-      ],
+      ], //main children
     );
   }
+}
+
+class Item {
+  String image;
+  String title;
+
+  Item({this.image, this.title});
 }
