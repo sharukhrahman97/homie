dd8d0c8d3c41a1708a2ae73de5d9df8b14e41741
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-12 18:39:06 +0530

    home partial complete

--- lib/Filter.dart
+++ lib/Filter.dart
@@ -42,13 +42,13 @@ class _FilterItemsState extends State<FilterItems> {
   //for gridview
   Item item1 = new Item(image: "AC", title: "assets/oven.png");
   Item item2 = new Item(image: "WI-FI", title: "assets/wifi.png");
-  Item item3 = new Item(image: "Fridge", title: "assets/fridge.png");
+  /* Item item3 = new Item(image: "Fridge", title: "assets/fridge.png");
   Item item4 = new Item(image: "Washing Machine", title: "assets/heater.png");
   Item item5 = new Item(image: "Water Heater", title: "assets/heater.png");
   Item item6 = new Item(image: "T.V", title: "assets/tv.png");
   Item item7 = new Item(image: "Microwave Oven", title: "assets/oven.png");
   Item item8 = new Item(image: "R.O Water", title: "assets/ro.png");
-  Item item9 = new Item(image: "Parking", title: "assets/parking.png");
+  Item item9 = new Item(image: "Parking", title: "assets/parking.png"); */
   //end of grid view vars
   List<bool> isSelected = [false, false, false];
   RangeValues _values = RangeValues(5, 60); //for budget slider
@@ -57,20 +57,27 @@ class _FilterItemsState extends State<FilterItems> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
+
     List<Item> myList = [
       item1,
       item2,
-      item3,
+      /*  item3,
       item4,
       item5,
       item6,
       item7,
       item8,
-      item9
+      item9 */
     ];
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
       children: [
         Container(
             child: Column(
@@ -151,6 +158,7 @@ class _FilterItemsState extends State<FilterItems> {
         ),
         Container(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               Text("Sex"),
               RadioListTile(
@@ -176,16 +184,19 @@ class _FilterItemsState extends State<FilterItems> {
             ],
           ),
         ),
-        Container(
+        /* Container(
           child: Column(
             children: [
               Text("Ammeneties"),
-              Flexible(
+              new Expanded(
                 child: GridView.count(
-                  childAspectRatio: 1.0,
+                  childAspectRatio: (itemWidth / itemHeight),
+                  controller: new ScrollController(keepScrollOffset: false),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
                   crossAxisCount: 3,
                   children: myList.map((data) {
-                    return Container(
+                    return Center(
                       child: Column(
                         children: [
                           Image.asset(data.image, width: 20),
@@ -198,7 +209,7 @@ class _FilterItemsState extends State<FilterItems> {
               )
             ],
           ),
-        )
+        ) */
       ], //main children
     );
   }
--- lib/Rooms.dart
+++ lib/Rooms.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'reuseable_search_widget.dart';
-void main() => runApp(Rooms());
 
+void main() => runApp(Rooms());
 
 class Rooms extends StatefulWidget {
   @override
@@ -12,7 +12,113 @@ class _RoomsState extends State<Rooms> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SearchCard(),
+        body: new Column(
+      children: [
+        Expanded(child: SearchCard(), flex: 1),
+        Expanded(
+          child: DummyList(),
+          flex: 4,
+        )
+      ],
+    ));
+  }
+}
+
+class DummyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(4.0),
+      itemBuilder: (context, i) {
+        return new Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 0,
+            margin: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://picsum.photos/400/300?random=1',
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text("HOMY GRG1507 Sector 01"),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text("Jharsa Village, Sector 01, Gurgaon",textScaleFactor: .8,),
+                  ),
+                ),
+                Container(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Container(child: Text("unisex")),
+                        Icon(Icons.wc)
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(child: Text("Occupancy")),
+                        Container(
+                          child: Text("Shared & Private"),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(child: Text("Hosted by")),
+                        Icon(Icons.person)
+                      ],
+                    )
+                  ],
+                )),
+                Container(
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text("Starting at"),
+                          Text("RS:5000"),
+                        ],
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          /*.. */
+                        },
+                        color: Colors.purple[900],
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(5.0)),
+                        child: Text("Schedule a Visit"),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ));
+      },
     );
   }
 }
