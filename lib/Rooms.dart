382da0b8aab39786dfeec6256cf11f4b4d21ba52
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2020-04-16 22:40:12 +0530

    changes in rooms

--- lib/Rooms.dart
+++ lib/Rooms.dart
@@ -63,141 +63,144 @@ class _DummyListState extends State<DummyList> {
   Widget build(BuildContext context) {
     return new ListView.builder(
       itemBuilder: (BuildContext context, i) {
-        return Card(
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                    child: PageView.builder(
-                        controller: PageController(viewportFraction: 0.7),
-                        itemBuilder: (BuildContext context, int itemIndex) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage('assets/images/image1.jpg')),
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 250,
+                      child: PageView.builder(
+                          controller: PageController(viewportFraction: 0.9),
+                          itemBuilder: (BuildContext context, int itemIndex) {
+                            return Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/images/image1.jpg')),
+                                ),
                               ),
-                            ),
-                          );
-                        })
+                            );
+                          })
 
-                    /* child: GestureDetector(
-                    onHorizontalDragEnd: (DragEndDetails details) {
-                      if (details.velocity.pixelsPerSecond.dx > 0) {
-                        _previous();
-                      } else if (details.velocity.pixelsPerSecond.dx < 0) {
-                        _next();
-                      }
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(images[currentIndex]),
-                            fit: BoxFit.cover,
+                      /* child: GestureDetector(
+                      onHorizontalDragEnd: (DragEndDetails details) {
+                        if (details.velocity.pixelsPerSecond.dx > 0) {
+                          _previous();
+                        } else if (details.velocity.pixelsPerSecond.dx < 0) {
+                          _next();
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(images[currentIndex]),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ), */
-                    ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Container(
-                    alignment: Alignment.centerLeft,
+                    ), */
+                      ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
                     child: Container(
-                      child: Text("HOMY GRG1507 Sector 1$i"),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      child: Text(
-                        "Jharsa Village, Sector 1$i, Gurgaon",
-                        textScaleFactor: .8,
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        child: Text("HOMY GRG1507 Sector 1$i"),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Container(child: Text("unisex")),
-                          Icon(Icons.wc)
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(child: Text("Occupancy")),
-                          Container(
-                            child: Text("Shared & Private"),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(child: Text("Hosted by")),
-                          Icon(Icons.person)
-                        ],
-                      )
-                    ],
-                  )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                        side: BorderSide(
-                          width: 1,
-                          color: Colors.grey,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        child: Text(
+                          "Jharsa Village, Sector 1$i, Gurgaon",
+                          textScaleFactor: .8,
                         ),
                       ),
                     ),
-                    child: Row(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Column(
                           children: [
-                            Text("Starting at"),
-                            Text("RS:5000"),
+                            Container(child: Text("unisex")),
+                            Icon(Icons.wc)
                           ],
                         ),
-                        FlatButton(
-                          onPressed: () {
-                            /*.. */
-                          },
-                          color: Colors.purple[900],
-                          textColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(5.0)),
-                          child: Text("Schedule a Visit"),
+                        Column(
+                          children: [
+                            Container(child: Text("Occupancy")),
+                            Container(
+                              child: Text("Shared & Private"),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(child: Text("Hosted by")),
+                            Icon(Icons.person)
+                          ],
                         )
                       ],
-                    ),
+                    )),
                   ),
-                )
-              ],
-            ));
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          side: BorderSide(
+                            width: 1,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text("Starting at"),
+                              Text("RS:5000"),
+                            ],
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              /*.. */
+                            },
+                            color: Colors.purple[900],
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(5.0)),
+                            child: Text("Schedule a Visit"),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        );
       },
     );
   }
